ef64a9de68a6e10c18ca4ab3a4f2c55096af5dee
gary804 <gary.guolin.jin@gmail.com>
2015-07-22 11:48:23 -0700

    update derivedclass.cpp

--- derivedclass.cpp
+++ derivedclass.cpp
@@ -6,11 +6,16 @@ class Polygon {
   protected:
     int width, height;
   public:
-    Polygon() {cout<< "Polygon: No parameter\n";}
+    Polygon() {cout<< "Polygon constructor: No parameter\n";}
     //Polygon(int a, int b) {cout<< "Polygon: 2 int parameters\n"; set_values(a,b);}  //works
-    Polygon(int a, int b) : width(a), height(b) {cout<< "Polygon: 2 int parameters: " <<a <<", " <<b <<"\n";}  //works
+    Polygon(int a, int b) : width(a), height(b) {cout<< "Polygon constructor: 2 int parameters: " <<a <<", " <<b <<"\n";}  //works
     void set_values (int a, int b)
       { width=a; height=b;}
+    //virtual member: A virtual member is a member function that can be redefined in a derived class, while preserving its calling properties through references.
+    //virtual int area() {return 0;}  //if called, it will call the member of the class that is pointed by a pointer. it just works like in Java
+    //virtual int area() =0;  //now it is a pure virtual function, and the class is a abstract class that will not allow to instantiate
+    virtual int area(void) =0;  //now it is a pure virtual function, and the class is a abstract class that will not allow to instantiate
+    void printarea(){ cout<<this->area() << endl;}  //this->area() will call subclass member function
  };
 
  class Output {
@@ -23,33 +28,52 @@ void Output::print (string str, int i) {
 
 class Rectangle: public Polygon , public Output {
   public:
-    Rectangle(){ cout<< "Rectangle: No parameter\n";}
+    Rectangle(){ cout<< "Rectangle constructor: No parameter\n";}
     //derived_constructor_name (parameters) : base_constructor_name (parameters) {...}
-    Rectangle(int a, int b) : Polygon(a,b) { cout<< "Rectangle: 2 int parameters: " <<a <<", " <<b <<"\n";}
+    Rectangle(int a, int b) : Polygon(a,b) { cout<< "Rectangle constructor: 2 int parameters: " <<a <<", " <<b <<"\n";}
     int area ()
       { return width * height; }
  };
 
 class Triangle: public Polygon, public Output {
   public:
-    Triangle(){cout<<"Triangle: No parameter\n";}
-    Triangle(int a, int b):Polygon(a,b){cout<<"Triangle: 2 int parameters: " <<a <<", " <<b <<"\n";}
+    Triangle(){cout<<"Triangle constructor: No parameter\n";}
+    Triangle(int a, int b):Polygon(a,b){cout<<"Triangle constructor: 2 int parameters: " <<a <<", " <<b <<"\n";}
     int area ()
       { return width * height / 2; }
   };
   
 int main () {
   {
+    cout << "Testing Group 1\n";
     Rectangle rect;
     Triangle trgl;
-    rect.set_values (4,5);
-    trgl.set_values (4,5);
+    //Polygon poly;
+    Polygon * ppolygon1, * ppolygon2, * ppolygon3; //pay attention on * here
+    ppolygon1 = &rect;
+    ppolygon2 = &trgl;
+    //ppolygon3 = &poly;
+    //rect.set_values (4,5);
+    //trgl.set_values (4,5);
+    ppolygon1->set_values (4,5);
+    ppolygon2->set_values (4,5);
+    //ppolygon3->set_values (4,5);
     //cout <<"Rectangle area is " << rect.area() << '\n';
     //cout <<"Triangle area is " << trgl.area() << '\n';
-    rect.print("Rectangle area is ", rect.area());
-    trgl.print("Triangle area is ", trgl.area());
+    //rect.print("Rectangle area is ", rect.area());  //works
+    //trgl.print("Triangle area is ", trgl.area()); //works
+    //rect.print("Rectangle area is ", ppolygon1->area());  //it won't work, because area is not a member in Polygon without virtual member
+    //trgl.print("Triangle area is ", ppolygon2->area());  //it won't work, only the members inherited from Polygon can be accessed
+    rect.print("Rectangle area is ", ppolygon1->area());  //now works, because it is virtual member
+    trgl.print("Triangle area is ", ppolygon2->area());  //now works
+    cout << "using printarea function\n";
+    cout<<"Rectangle area is "; ppolygon1->printarea();  //now works, because it is virtual member
+    cout<<"Triangle area is "; ppolygon2->printarea();  //now works
+    //cout <<"Polygon area is " << ppolygon3->area() <<endl;  //now works
   }
+
   {
+    cout << "Testing Group 2\n";
     Rectangle rect(4,5);
     Triangle trgl(4,5);
     //cout <<"Rectangle area is " << rect.area() << '\n';
@@ -57,6 +81,16 @@ int main () {
     rect.print("Rectangle area is ", rect.area());
     //trgl.print("Triangle area is ", trgl.area());  //works
     Triangle::print("Triangle area is ", trgl.area());
+  }
+  {
+    cout << "Testing Group 3\n";
+//following 2 lines are declared being of type "pointer to Polygon", but the objects allocated have been declared having the derived class type directly (Rectangle and Triangle).
+    Polygon * ppoly1 = new Rectangle (4,5);
+    Polygon * ppoly2 = new Triangle (4,5);
+    ppoly1->printarea();
+    ppoly2->printarea();
+    delete ppoly1;
+    delete ppoly2;
   }
   return 0;
 }
